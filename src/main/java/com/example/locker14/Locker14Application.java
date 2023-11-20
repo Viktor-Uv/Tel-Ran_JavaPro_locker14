218c3c6555be755e227d752820c68d2ff03b1218
Viktor-Uv <sev-vic@yandex.com>
2023-11-20 20:51:29 +0100

    Initial commit

---
+++ src/main/java/com/example/locker14/Locker14Application.java
@@ -0,0 +1,13 @@
+package com.example.locker14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Locker14Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Locker14Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/locker14/Locker14ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.locker14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Locker14ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
